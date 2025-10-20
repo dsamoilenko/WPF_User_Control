023f7c1c4e2f93bc14e08851b2e9b490769f326f
dsamoilenko <dsamoilenko@yahoo.com>
2025-10-20 17:31:18 +0300

    Add IPrintable

---
+++ WPF_User_Control/IPrintable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_User_Control
+{
+    public interface IPrintable
+    {
+        void Print(string text);
+        void Print(string text, string format);
+        void Print();
+    }
+}
