e40e22f139d45e518f0c5cd6aa6a59faf9fb3900
dsamoilenko <dsamoilenko@yahoo.com>
2025-10-20 17:58:41 +0300

    IPrintable, MyPrint changed

--- WPF_User_Control/IPrintable.cs
+++ WPF_User_Control/IPrintable.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace WPF_User_Control
 {
     public interface IPrintable
     {
         void Print(string text);
-        void Print(string format, string text);
-        void Print();
-        bool TryToPrint();
+        void Print(string text, string format);
+        void Print(BitmapImage image, BitmapDecoder decoder);
+        bool TryToPrint(string text);
     }
 }
--- WPF_User_Control/MyPrint.cs
+++ WPF_User_Control/MyPrint.cs
@@ -3,29 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace WPF_User_Control
 {
     public class MyPrint : IPrintable
     {
         public void Print(string text)
-        {
-            throw new NotImplementedException();
-        }
+        {}
 
         public void Print(string format, string text)
-        {
-            throw new NotImplementedException();
-        }
+        {}
 
-        public void Print()
-        {
-            throw new NotImplementedException();
-        }
+        void IPrintable.Print(BitmapImage image, BitmapDecoder decoder)
+        {}
 
-        public bool TryToPrint()
+        bool IPrintable.TryToPrint(string text)
         {
-            throw new NotImplementedException();
+            return false;
         }
     }
 }
